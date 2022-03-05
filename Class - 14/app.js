e2cd9a12353970497ddea98f1b6c46eb23961c72
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-05 18:42:04 +0500

    Update

---
+++ Class - 14/app.js
@@ -0,0 +1,23 @@
+    // var a= new Date();
+
+    // document.write(a);
+
+    //  document.write("<br>")    
+
+    // var b= a.getDate();
+    //  document.write(b);
+
+    //  document.write("<br>")
+
+    // var c= a.getDay();
+    //  document.write(c);
+
+    function add(a){
+        alert(a)
+        
+    }
+   
+    add("Hello World");
+     
+
+
