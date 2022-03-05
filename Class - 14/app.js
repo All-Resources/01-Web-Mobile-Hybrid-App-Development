00aece0e5ee27523c75107cff573fa38a0d2f5c4
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-05 21:32:06 +0500

    update code

--- Class - 14/app.js
+++ Class - 14/app.js
@@ -12,12 +12,66 @@
     // var c= a.getDay();
     //  document.write(c);
 
-    function add(a){
-        alert(a)
+    // function add(a,b){
+    //     return "Hello World";
         
-    }
+    // }
    
-    add("Hello World");
-     
+    // document.write(add(3,4))
+
+    // Create a Calculator
 
 
+    // function cal(num1,opr, num2){
+
+    //     if(opr=== "+"){
+
+    //         return num1 + num2
+    //     }
+    //     else if (opr==="-"){
+
+    //         return num1 - num2
+    //     }
+    //     else if (opr==="*"){
+
+    //         return num1 * num2
+
+    //     }
+    //     else{
+    //         return "Incorrect Operator"
+    //     }
+    //     }
+    //    var result1 = cal(4, "-" , 2);
+    //    var result2 = cal(45,"+" , 12);
+    //    var result3= cal(23,"*" ,12);
+    //    console.log(result1);
+    //    console.log(result2);
+    //    console.log(result3);
+
+
+    //    function m(a,b=1){
+    //        return a+b;
+    //    }
+    //    alert(m(5,2));
+
+    //    function m(a,b){
+    //        return a+b;
+
+    //    }
+    //    console.log(m(2,3))
+
+
+    // Switch Statements
+
+    let name="Raa";
+    switch(name){
+        case "Raza":
+            document.write("My Name is Raza")
+            break;
+        case "Ali":
+            document.write("My Name is Ali")
+            break;
+         default:
+            document.write("Enter Valid Value")
+
+    }
