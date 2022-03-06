ff5f7f1a239d59bd22f9e1b38ce2d7d8a3dad482
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-07 00:34:13 +0500

    update

---
+++ Class - 15/app.js
@@ -0,0 +1,28 @@
+// for(var a = 0; a <= 10; a++)
+// {
+// document.write(a +"<br>");
+
+// }
+
+// document.write("<br> <br>")
+
+
+// var b = 0;
+// while(b<10){
+//     document.write(b);
+//     b++;
+// }
+
+
+var c = 0;
+do{
+    document.write(c +"<br>");
+    c++;
+}
+while(c<10)
+
+
+
+
+
+
