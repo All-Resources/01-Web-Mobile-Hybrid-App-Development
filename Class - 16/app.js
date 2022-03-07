38f6da1a3f3a73f79586a35e7a61bdca02c86d93
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-07 13:06:18 +0500

    Update

---
+++ Class - 16/app.js
@@ -0,0 +1,8 @@
+function readMore(){
+    // var text = "Lorem ipsum dolor Totam in nulla Muhammad Ahsan"
+    var text = document.getElementById('text').innerHTML; 
+    
+    var peragraf = document.getElementById('pera');
+
+    peragraf.innerHTML = text;
+}
