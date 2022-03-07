1da318d7e3ec89b1ff4ba6a58a35259d62b9df79
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-07 14:59:56 +0500

    update

--- Class - 16/app.js
+++ Class - 16/app.js
@@ -1,9 +1,31 @@
-function readMore(){
-    var text = "<ul><li>List 1</li><li>List 2</li><li>List 3</li><li>List 4 </li></ul>"
+// function readMore(){
+//     var text = "<ul><li>List 1</li><li>List 2</li><li>List 3</li><li>List 4 </li></ul>"
 
-    // var text = document.getElementById('text').innerHTML; 
+//     // var text = document.getElementById('text').innerHTML; 
     
-    var peragraf = document.getElementById('pera');
+//     var peragraf = document.getElementById('pera');
 
-    peragraf.innerHTML = text;
+//     peragraf.innerHTML = text;
+// }
+
+
+// function big(){
+//     var img = document.getElementById('car')
+//     img.className ='hidden'
+// }
+
+// function changeimg(){
+//     var a = document.getElementById('img') 
+//     a.src="https://images.unsplash.com/photo-1494633114655-819eb91fde40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhkJTIwaW1hZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+// }
+
+
+// function changeimgbefore(){
+//     var b = document.getElementById('img') 
+//     b.src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+// }
+
+function changeimage(id,src){
+var image = document.getElementById(id);
+image.src = src
 }
