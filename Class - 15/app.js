ba37b47b6228e538b3d434f3b2a6fcd6fe3477de
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-07 11:51:22 +0500

    update

--- Class - 15/app.js
+++ Class - 15/app.js
@@ -56,10 +56,19 @@
 // Calculator
 
 
-function getNumber(num){
-    console.log(num)
-    var result =document.getElementById("result");
+function getNumber(num) {
+var result = document.getElementById("result");
+    result.value += num;
 }
 
 
 
+function clearResult() {
+    var result = document.getElementById("result");
+    result.value ="";
+}
+
+function getResult(){
+var result = document.getElementById("result");
+result.value=eval(result.value)
+}
