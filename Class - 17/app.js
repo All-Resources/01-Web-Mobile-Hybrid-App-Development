e082b5793282e127aff40aee32d75080e657a99f
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-08 10:48:51 +0500

    Update app.js

--- Class - 17/app.js
+++ Class - 17/app.js
@@ -1,4 +1,21 @@
+// function timer(){
+//     console.log("running..")
+// }
+// setInterval(timer, 1000)
+
+
+let count =0;
+let interval;
 function timer(){
-    console.log("running..")
-}
-setInterval(timer, 1000)
+    count++
+    document.write(count + "<br>")
+} 
+ interval= setInterval(timer, 1000)
+ setTimeout(function(){
+  clearInterval(interval)
+ },5000) 
+
+// function timeOut(){
+//     console.log("running")
+// }
+// setTimeout(timeOut, 3000)
