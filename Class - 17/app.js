4ade09e67c67d03823562dda74f502a2a1ec75a0
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-08 11:26:13 +0500

    update

--- Class - 17/app.js
+++ Class - 17/app.js
@@ -3,19 +3,36 @@
 // }
 // setInterval(timer, 1000)
 
-
-let count =0;
-let interval;
-function timer(){
-    count++
-    document.write(count + "<br>")
-} 
- interval= setInterval(timer, 1000)
- setTimeout(function(){
-  clearInterval(interval)
- },5000) 
+// let count = 0;
+// let interval;
+// function timer() {
+//   count++;
+//   document.write(count + "<br>");
+// }
+// interval = setInterval(timer, 1000);
+// setTimeout(function () {
+//   clearInterval(interval);
+// }, 5000);
 
 // function timeOut(){
 //     console.log("running")
 // }
 // setTimeout(timeOut, 3000)
+
+
+
+
+let min = 0;
+let sec = 0;
+let msec = 0;
+
+let minHeading = document.getElementById('min');
+let secHeading = document.getElementById('sec');
+let msecHeading = document.getElementById('msec');
+let interval;
+
+        function timer(){
+        msec++
+        msecHeading.innerHTML=msec;
+}
+        setInterval(timer, 100)
