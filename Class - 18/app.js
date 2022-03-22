f5b34a3925d6679390d909ea7c4333a782662e4c
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-22 15:44:55 +0500

    update

--- Class - 18/app.js
+++ Class - 18/app.js
@@ -1,14 +1,26 @@
-function disablebtn(){
+// function disablebtn(){
 
-    let btn =document.getElementById("disable");
+//     let btn =document.getElementById("disable");
 
-    btn.disabled = true ;
-}
+//     btn.disabled = true ;
+// }
 
 
-function getElements(){
-    // var p = document.getElementsByTagName('p');
-    // console.log(p);
-    console.log(document.childNodes[0])
+// function getElements(){
+//     // var p = document.getElementsByTagName('p');
+//     // console.log(p);
+//     console.log(document.childNodes[0])
+
+// }
+
+
+
+function addmessage(){
+    var val =document.getElementById("val");
+    var p =document.createElement("p")
+    var text = document.createTextNode(val.value)
+    p.appendChild(text)
+    var messages =document.getElementById("messages") 
+    messages.appendChild(p)
 
 }
